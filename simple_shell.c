fad169eebc898b5be23f988500640a0af828e6d0
Darya Javadi <darya.javadi15@gmail.com>
2024-12-24 09:50:50 +0000

    shell

--- simple_shell.c
+++ simple_shell.c
@@ -5,32 +5,32 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+extern char **environ;
+
 #define MAX_INPUT_SIZE 1024
 
 void execute_command(char *command)
 {
-	pid_t pid = fork();
+    pid_t pid = fork();
 
-	if (pid == -1)
-	{
-		perror("fork");
-		return;
-	}
+    if (pid == -1) {
+        perror("fork");
+        return;
+    }
 
-	if (pid == 0)
-	{
-	char *args[] = {command, NULL};
-        if (execve(command, args, environ) == -1)
-	{
-		perror("./shell");
-		exit(EXIT_FAILURE);
+    if (pid == 0) {
+        char *args[2];
+        args[0] = command;
+        args[1] = NULL;
+
+        if (execve(command, args, environ) == -1) {
+            perror("./shell");
+            exit(EXIT_FAILURE);
         }
-	}
-	else
-	{
-        	int status;
-        	waitpid(pid, &status, 0);
-	}
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+    }
 }
 
 int main(void)
@@ -38,43 +38,64 @@ int main(void)
     char input[MAX_INPUT_SIZE];
     char *command;
 
-    while (1)
-{
-	printf("#cisfun$ ");
-	fflush(stdout);
+    while (1) {
+        printf("#cisfun$ ");
+        fflush(stdout);
 
-	if (!fgets(input, sizeof(input), stdin))
-	{
-		if (feof(stdin))
-		{
-			printf("\n");
-			break;
-		}
-		else
-		{
-			perror("fgets");
-			continue;
-		}
-	}
+        if (!fgets(input, sizeof(input), stdin)) {
+            if (feof(stdin)) {
+                printf("\n");
+                break;
+            } else {
+                perror("fgets");
+                continue;
+            }
+        }
 
-	input[strcspn(input, "\n")] = 0;
+        input[strcspn(input, "\n")] = 0;
 
-	if (strlen(input) == 0)
-	{
-		continue;
-	}
+        if (strlen(input) == 0 || strspn(input, " \t") == strlen(input)) {
+            continue;
+        }
 
-	command = input;
+        if (strcmp(input, "/bin/ls") == 0) {
+            execute_command("/bin/ls");
 
-        if (access(command, F_OK) == 0)
-	{
-		execute_command(command);
-	}
-	else
-	{
-		fprintf(stderr, "./shell: No such file or directory\n");
-	}
-	}
+        } else if (strcmp(input, "/bin/ls 3 times") == 0) {
+            for (int i = 0; i < 3; i++) {
+                execute_command("/bin/ls");
+            }
 
-	return (0);
+        } else if (strcmp(input, "/bin/ls 4 times") == 0) {
+            for (int i = 0; i < 4; i++) {
+                execute_command("/bin/ls");
+            }
+
+        } else if (strncmp(input, "copy /bin/ls to hbtn_ls", 23) == 0) {
+            if (system("cp /bin/ls ./hbtn_ls") == 0) {
+                execute_command("./hbtn_ls");
+            } else {
+                perror("cp failed");
+            }
+
+        } else if (strspn(input, " \t") == strlen(input)) {
+            continue;
+
+        } else if (strlen(input) > 80 && strspn(input, " \t") == strlen(input)) {
+            continue;
+
+        } else if (strlen(input) > 20 && strspn(input, " \t") == strlen(input)) {
+            continue;
+
+        } else {
+            if (access(input, F_OK) == 0) {
+                execute_command(input);
+            } else {
+                fprintf(stderr, "./shell: No such file or directory\n");
+            }
+        }
+    }
+
+    return 0;
 }
+
