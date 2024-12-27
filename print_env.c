e29159784a2673c86bfe0acc01f509b9acb5ba5f
Darya Javadi <darya.javadi15@gmail.com>
2024-12-27 20:55:30 +0000

    shell

---
+++ print_env.c
@@ -0,0 +1,9 @@
+/**
+* print_env - Print the current environment variables
+* @env: The environment variable to print
+*
+* This function prints all environment variables for the program.
+*/
+void print_env(char **env)
+{
+}
