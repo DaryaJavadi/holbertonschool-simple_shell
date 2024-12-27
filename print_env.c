b62389fc28cb7d3a0edfac563eac15e97ba7b506
Darya Javadi <darya.javadi15@gmail.com>
2024-12-27 21:02:21 +0000

    shell

--- print_env.c
+++ print_env.c
@@ -1,23 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * print_env - prints all environment variables
+ * execute - executes the given program or handles built-ins
+ * @argv: argument vector
+ * @status: variable for exit status
  */
-void print_env(void)
+void execute(char **argv, int *status)
 {
-    extern char **environ;
-    char **env = environ;
+    pid_t pid;
+    char *command;
 
-    if (!env || !*env)
+    if (argv[0] == NULL)
+        return;
+
+    if (strcmp(argv[0], "exit") == 0)
     {
+        free(argv);
+        exit(*status);
+    }
+
+    if (strcmp(argv[0], "env") == 0)
+    {
+        print_env();
+        *status = 0;
         return;
     }
 
-    while (*env)
+    command = get_full_path(argv[0], status);
+    if (command == NULL)
+        return;
+
+    pid = fork();
+    if (pid == 0)
     {
-        printf("%s\n", *env);
-        env++;
+        if (execve(command, argv, environ) == -1)
+        {
+            perror("Error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid > 0)
+    {
+        wait(NULL);
+        free(command);
+    }
+    else
+    {
+        perror("Fork failed");
+        free(command);
     }
 }
 
