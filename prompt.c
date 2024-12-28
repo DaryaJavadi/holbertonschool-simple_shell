7984576c479750afbf20be692fbd31557fa01480
Darya Javadi <darya.javadi15@gmail.com>
2024-12-28 20:47:48 +0000

    simple_shell

---
+++ prompt.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * prompt - function to print prompt
+ *
+ * Return: na
+ */
+void prompt(void)
+{
+	write(1, "$ ", 3);
+}
---
+++ shell.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * main - primary function with no args
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char **argv, char **env)
+{
+	if (argc == 1 || argv[1] == NULL)
+		looper(env);
+
+	return (0);
+}
