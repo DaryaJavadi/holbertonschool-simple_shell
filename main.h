d5465e0396f9b91d4b5236dfb16d6fecb4df568c
Darya Javadi <darya.javadi15@gmail.com>
2024-12-29 00:46:59 +0000

    shell

--- main.h
+++ main.h
@@ -1,34 +1,28 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-#include <sys/wait.h>
-#include <sys/types.h>
+#ifndef MAINH
+#define MAINH
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include <dirent.h>
-#include <limits.h>
-#include <string.h>
 
+#define MAX_LEN 100
+#define PROMPT ">> "
+#define PATH "PATH="
 extern char **environ;
+void handle_command(char *u_command);
+void print_env(void);
+void setup_environment(char **path_env, char **path);
+void execute_command(char **args, char *path);
+void parse_command(char *u_command, char **args);
+void handle_path(char **args, char **path, char **path_env, int *found);
+void handle_command(char *u_command);
+void process_commands(char *commands, char **commands_array);
+void handle_commands_array(char **commands_array);
 
-char *show_input(void);
-void prompt(void);
-char *_strcat(char *src);
-int _strlen(char *str);
-void place(char *str);
-char *findfile(char *command);
-char *find_command(char *command);
-int compare(char *s1, char *s2);
-int _strcmpdir(char *s1, char *s2);
-int charput(char c);
-void place(char *str);
-char *str_concat(char *s1, char *s2);
-int lookforslash(char *cmd);
-int compareExit(char *s1, char *s2);
-int compareEnv(char *s1, char *s2);
-void execute_proc(char **cmd);
-char **identify_string(char *parameter);
-void controlC(int sig);
 #endif
